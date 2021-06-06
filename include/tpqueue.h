c15b33513957af721217505606406ea712edda9a
spas1isohrani <78902102+spas1isohrani@users.noreply.github.com>
2021-06-06 18:02:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,7 +5,53 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+ private: 
+  T core;
+  TPQueue* next;
+  TPQueue* prev;
+  TPQueue* start;
+
+ public:
+  TPQueue() {
+    next = nullptr;
+    prev = nullptr;
+    start = nullptr;
+  }
+  
+  void push(const T &element) {
+    if (start != nullptr) {
+      TPQueue* step = new TPQueue;
+      step->core = element;
+      step->prev = prev;
+      prev = step;
+      next->next = step;
+      next = step;
+      if (step->core.prior > step->prev->core.prior) {
+        TPQueue* step_prev = step->prev;
+        TPQueue* step_this = step;
+        while (step_prev && step_this->core.prior > step_prev->core.prior) {
+          T x = step_this->core;
+          step_this->core = step_prev->core;
+          step_prev->core = x;
+          step_this = step_this->prev;
+          step_prev = step_prev->prev;
+        }
+      }
+    } else {
+      TPQueue* step = new TPQueue;
+      step->core = element;
+      start = step;
+      prev = step;
+      next = step;
+    }
+  }
+  T pop() {
+    if (start != nullptr) {
+      T x = start->core;
+      start = start->next;
+      return x;
+    }
+  }
 };
 
 struct SYM {
